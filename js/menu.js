332404f2a0ab9e450f27ece548a6fd784d4373f1
Jackortega922 <jackortega922@gmail.com>
2026-07-16 12:23:23 -0500

    Implementa navbar modular

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+import { initMenu } from "./menu.js";
+
+export function initMenu() {
+    const menuButton = document.querySelector("#menuBtn");
+    const mobileMenu = document.querySelector("#mobileMenu");
+    const menuLinks = mobileMenu.querySelectorAll("a");
+
+    if (!menuButton || !mobileMenu) return;
+
+    // Abrir / cerrar menú
+    menuButton.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
+
+    // Cerrar menú al seleccionar una opción
+    menuLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            mobileMenu.classList.add("hidden");
+        });
+    });
+}
